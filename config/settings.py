478c7959ec75c95cba95906e8a7aa033cf3ea7e9
akshat45 <gargakshat952@gmail.com>
2026-01-19 10:54:40 +0530

    added domain to run the service

--- config/settings.py
+++ config/settings.py
@@ -5,9 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i-e$)v725)hfb" \
              "^cvjas_(no006@-yguf+2-n$l_n&=h_-)y$4f"
 
-DEBUG = True
+import os
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eventgraphia-assignment-production.up.railway.app', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
